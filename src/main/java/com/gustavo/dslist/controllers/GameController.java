495ac1a75ee501fa8a82c1ca2703e8b7ef9aa461
Gustavo <gpontesf06@gmail.com>
2023-07-10 22:28:23 -0300

    repository, service e controller

---
+++ src/main/java/com/gustavo/dslist/controllers/GameController.java
@@ -0,0 +1,24 @@
+package com.gustavo.dslist.controllers;
+
+import com.gustavo.dslist.dto.GameMinDTO;
+import com.gustavo.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import com.gustavo.dslist.entities.Game;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll(){
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+}
---
+++ src/main/java/com/gustavo/dslist/services/GameService.java
@@ -0,0 +1,19 @@
+package com.gustavo.dslist.services;
+
+import com.gustavo.dslist.dto.GameMinDTO;
+import com.gustavo.dslist.entities.Game;
+import com.gustavo.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+    @Autowired
+    private GameRepository gameRepository;
+    public List<GameMinDTO> findAll(){
+        List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+}
