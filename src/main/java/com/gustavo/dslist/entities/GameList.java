73b7f75e55decc47597e5f412f50039b6bc62c1b
Gustavo <gpontesf06@gmail.com>
2023-07-11 22:20:24 -0300

    Modelo de domínio implementado

---
+++ src/main/java/com/gustavo/dslist/entities/GameList.java
@@ -0,0 +1,52 @@
+package com.gustavo.dslist.entities;
+
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public GameList() {
+    }
+
+    public GameList(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameList gameList = (GameList) o;
+        return id.equals(gameList.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
